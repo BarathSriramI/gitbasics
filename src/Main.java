53ab7a6a1ee480eddc9d1936c185a5641a092c04
barath sriram <barathsriramiyyappan@gmail.com>
2023-06-11 21:31:48 +0530

    added variables

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("hi ");
         System.out.println("added new lines");
+        System.out.println("hello");
+
+        int x=1,y=-2;
 
     }
 }
